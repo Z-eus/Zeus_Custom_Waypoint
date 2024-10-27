d765f714dfe20febe32d025918e82fda1b0f4496
Sadicius <124639760+Sadicius@users.noreply.github.com>
2024-10-27 15:44:18 +0100

    clean and littel change in stop resource

--- client/client.lua
+++ client/client.lua
@@ -6,19 +6,19 @@ Citizen.CreateThread(function()
 
         if IsWaypointActive() then
             local WaypointCoords = GetWaypointCoords()
-			
+
             SetWaypointOff()
-			
+
             ClearGpsMultiRoute()
-			
+
             StartGpsMultiRoute(GetHashKey(Config.WaypointColor), true, true)
-			
+
             AddPointToGpsMultiRoute(WaypointCoords.x, WaypointCoords.y, WaypointCoords.z)
-			
+
 			if blip then
 				RemoveBlip(blip)
 			end
-			
+
 			if Config.AddBlipWaypointEndPoint then
 				blip = N_0x554d9d53f696d002(1664425300, WaypointCoords.x, WaypointCoords.y, WaypointCoords.z)
 				SetBlipSprite(blip, Config.BlipSprite, true)
@@ -26,36 +26,40 @@ Citizen.CreateThread(function()
 				Citizen.InvokeNative(0x9CB1A1623062F402, blip, Config.BlipName)
 				BlipAddModifier(blip, GetHashKey(Config.BlipColor))
 			end
-			
+
             SetGpsMultiRouteRender(true)
-			
+
         end
     end
 end)
 
 RegisterCommand(Config.WaypointClearCommand, function() -- Clear/Remove Route
 
-		SetWaypointOff()
-		ClearGpsMultiRoute()
-		SetGpsMultiRouteRender(false)
-		
-		if blip then
+	SetWaypointOff()
+	ClearGpsMultiRoute()
+	SetGpsMultiRouteRender(false)
+
+	if blip then
 		RemoveBlip(blip)
 		blip = nil
-		end
-		
-		if Config.Notification.Enable then
+	end
+
+	if Config.Notification.Enable then
 		TriggerEvent("chat:addMessage", {color = Config.Notification.TitleColor, multiline = true, args = {Config.Notification.Title, Config.Notification.Text}}) -- You can use different notification scripts
-		end
-		
+	end
+
 end, false)
 
 AddEventHandler('onResourceStop', function(resourceName)
-    if GetCurrentResourceName() == resourceName then
+	local resource = GetCurrentResourceName()
+    if resource == resourceName then
 		SetWaypointOff()
 		ClearGpsMultiRoute()
 		SetGpsMultiRouteRender(false)
-		RemoveBlip(blip)
+		if blip then
+			RemoveBlip(blip)
+			blip = nil
+		end
 		--print("All waypoint/route things cleared")
     end
 end)
