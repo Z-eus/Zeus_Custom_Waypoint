0edd9b5f24f9d9f66ed878ff4ce3eab79d6f525b
Zeus Script <51821404+Z-eus@users.noreply.github.com>
2024-10-28 23:04:13 +0300

    Update client.lua
    - v0.1
        - Removed the “BLIP_NAME” text that appears with the Indicator after marking another location on the map
        - Added waypoint can be deleted with key combination
        - Small improvements

--- client/client.lua
+++ client/client.lua
@@ -1,61 +1,78 @@
 local blip = nil
 
-Citizen.CreateThread(function()
-    while true do
+CreateThread(function()
+	while true do
         Wait(Config.WaypointColorChangeDelay)
+	local WaypointCoords = GetWaypointCoords()
 
         if IsWaypointActive() then
-            local WaypointCoords = GetWaypointCoords()
-			
             SetWaypointOff()
-			
             ClearGpsMultiRoute()
-			
             StartGpsMultiRoute(GetHashKey(Config.WaypointColor), true, true)
-			
             AddPointToGpsMultiRoute(WaypointCoords.x, WaypointCoords.y, WaypointCoords.z)
-			
-			if blip then
-				RemoveBlip(blip)
-			end
-			
-			if Config.AddBlipWaypointEndPoint then
-				blip = N_0x554d9d53f696d002(1664425300, WaypointCoords.x, WaypointCoords.y, WaypointCoords.z)
-				SetBlipSprite(blip, Config.BlipSprite, true)
-				SetBlipScale(blip, 0.2)
-				Citizen.InvokeNative(0x9CB1A1623062F402, blip, Config.BlipName)
-				BlipAddModifier(blip, GetHashKey(Config.BlipColor))
-			end
+
+	    if blip then
+		RemoveBlip(blip)
+	    end
+
+	    if Config.AddBlipWaypointEndPoint then
+		blip = N_0x554d9d53f696d002(1664425300, WaypointCoords.x, WaypointCoords.y, WaypointCoords.z)
+		SetBlipSprite(blip, Config.BlipSprite, true)
+		SetBlipScale(blip, 0.2)
+		Citizen.InvokeNative(0x9CB1A1623062F402, blip, Config.BlipName)
+		BlipAddModifier(blip, GetHashKey(Config.BlipColor))
+	    end
 			
             SetGpsMultiRouteRender(true)
-			
         end
-    end
-end)
 
-RegisterCommand(Config.WaypointClearCommand, function() -- Clear/Remove Route
+	if IsUiappActiveByHash(`MAP`) ~= 0 then
+	   local mapFocus = DatabindingAddUiItemListFromPath("", "MapFocus")
+	   local hoveredName = DatabindingAddDataString(mapFocus, "HoveredName", "")
+	   DatabindingWriteDataString(hoveredName, "")
+	   DatabindingAddDataBool(mapFocus, "ItemHovered", false)
+	end
 
+	if Config.WaypointClearWithKey then
+	   if IsControlJustPressed(0,Config.WaypointClearKey) and IsControlJustPressed(0,Config.WaypointClearKey2) then
 		SetWaypointOff()
 		ClearGpsMultiRoute()
 		SetGpsMultiRouteRender(false)
-		
+
 		if blip then
 		RemoveBlip(blip)
 		blip = nil
 		end
-		
+
 		if Config.Notification.Enable then
-		TriggerEvent("chat:addMessage", {color = Config.Notification.TitleColor, multiline = true, args = {Config.Notification.Title, Config.Notification.Text}}) -- You can use different notification scripts
+		   TriggerEvent("chat:addMessage", {color = Config.Notification.TitleColor, multiline = true, args = {Config.Notification.Title, Config.Notification.Text}}) -- You can use different notification scripts
 		end
-		
+	    end
+	end
+    end
+end)
+
+RegisterCommand(Config.WaypointClearCommand, function() -- Clear/Remove Route
+	SetWaypointOff()
+	ClearGpsMultiRoute()
+	SetGpsMultiRouteRender(false)
+
+	if blip then
+	RemoveBlip(blip)
+	blip = nil
+	end
+
+	if Config.Notification.Enable then
+	TriggerEvent("chat:addMessage", {color = Config.Notification.TitleColor, multiline = true, args = {Config.Notification.Title, Config.Notification.Text}}) -- You can use different notification scripts
+	end
 end, false)
 
 AddEventHandler('onResourceStop', function(resourceName)
     if GetCurrentResourceName() == resourceName then
-		SetWaypointOff()
-		ClearGpsMultiRoute()
-		SetGpsMultiRouteRender(false)
-		RemoveBlip(blip)
+	SetWaypointOff()
+	ClearGpsMultiRoute()
+	SetGpsMultiRouteRender(false)
+	RemoveBlip(blip)
 		--print("All waypoint/route things cleared")
     end
 end)
