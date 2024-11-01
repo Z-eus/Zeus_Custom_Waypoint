1a9de54336bf5785091a1304687c6b9cd4f508ea
Zeus Script <51821404+Z-eus@users.noreply.github.com>
2024-11-01 13:06:45 +0300

    Update client.lua
    - v0.2
        - The waypoint will be automatically deleted when the character reaches the marked location
        - 3D marker can now be added to the location you mark on the map
        - Debug added

--- client/client.lua
+++ client/client.lua
@@ -1,61 +1,130 @@
 local blip = nil
+local waypoints = {}
+local marker = true
+
+function SaveWaypoint(coords)
+    if waypoints == nil then
+        waypoints = {}
+    end
+
+    if #waypoints > 0 then
+        waypoints[1] = {x = coords.x, y = coords.y, z = coords.z}
+
+	if Config.Debug then
+        print("Waypoint updated! New Coordinates:", coords.x, coords.y, coords.z)
+	end
+    else
+        table.insert(waypoints, {x = coords.x, y = coords.y, z = coords.z})
+
+	if Config.Debug then
+        print("Waypoint saved! Coordinates", coords.x, coords.y, coords.z)
+	end
+    end
+end
+
+function AddMarker(coords)
+     if marker then
+	local WaypointSaveCoord = vector3(waypoints[1].x, waypoints[1].y, waypoints[1].z)
+        Citizen.InvokeNative(0x2A32FAA57B937173,Config.MarkerStyle, WaypointSaveCoord.x, WaypointSaveCoord.y, WaypointSaveCoord.z - 1,0,0,0,0,0,0,1.0,1.0,500.0,Config.MarkerColor.r,Config.MarkerColor.g,Config.MarkerColor.b,Config.MarkerColor.a,0, 0, 2, 0, 0, 0, 0)
+    end
+end
 
 CreateThread(function()
-	while true do
+    while true do
         Wait(Config.WaypointColorChangeDelay)
-	local WaypointCoords = GetWaypointCoords()
-
+		
         if IsWaypointActive() then
+            local WaypointCoords = GetWaypointCoords()
+            SaveWaypoint(WaypointCoords)
             SetWaypointOff()
             ClearGpsMultiRoute()
             StartGpsMultiRoute(GetHashKey(Config.WaypointColor), true, true)
             AddPointToGpsMultiRoute(WaypointCoords.x, WaypointCoords.y, WaypointCoords.z)
 
-	    if blip then
-		RemoveBlip(blip)
-	    end
+            if blip then
+                RemoveBlip(blip)
+            end
 
-	    if Config.AddBlipWaypointEndPoint then
-		blip = N_0x554d9d53f696d002(1664425300, WaypointCoords.x, WaypointCoords.y, WaypointCoords.z)
-		SetBlipSprite(blip, Config.BlipSprite, true)
-		SetBlipScale(blip, 0.2)
-		Citizen.InvokeNative(0x9CB1A1623062F402, blip, Config.BlipName)
-		BlipAddModifier(blip, GetHashKey(Config.BlipColor))
-	    end
-			
+            if Config.AddBlipWaypointEndPoint then
+                blip = N_0x554d9d53f696d002(1664425300, WaypointCoords.x, WaypointCoords.y, WaypointCoords.z)
+                SetBlipSprite(blip, Config.BlipSprite, true)
+                SetBlipScale(blip, 0.2)
+                Citizen.InvokeNative(0x9CB1A1623062F402, blip, Config.BlipName)
+                BlipAddModifier(blip, GetHashKey(Config.BlipColor))
+            end
+            
             SetGpsMultiRouteRender(true)
+	    marker = true
         end
 
-	if IsUiappActiveByHash(`MAP`) ~= 0 then
-	   local mapFocus = DatabindingAddUiItemListFromPath("", "MapFocus")
-	   local hoveredName = DatabindingAddDataString(mapFocus, "HoveredName", "")
-	   DatabindingWriteDataString(hoveredName, "")
-	   DatabindingAddDataBool(mapFocus, "ItemHovered", false)
-	end
+        local PlayerCoords = GetEntityCoords(PlayerPedId())
+        if waypoints and #waypoints > 0 then
+            local WaypointSaveCoord = vector3(waypoints[1].x, waypoints[1].y, waypoints[1].z)
+            local distance = math.sqrt((PlayerCoords.x - WaypointSaveCoord.x)^2 + (PlayerCoords.y - WaypointSaveCoord.y)^2)
 
-	if Config.WaypointClearWithKey then
-	   if IsControlJustPressed(0,Config.WaypointClearKey) and IsControlJustPressed(0,Config.WaypointClearKey2) then
-		SetWaypointOff()
-		ClearGpsMultiRoute()
-		SetGpsMultiRouteRender(false)
-
-		if blip then
-		RemoveBlip(blip)
-		blip = nil
-		end
-
-		if Config.Notification.Enable then
-		   TriggerEvent("chat:addMessage", {color = Config.Notification.TitleColor, multiline = true, args = {Config.Notification.Title, Config.Notification.Text}}) -- You can use different notification scripts
-		end
+	   if Config.Debug then
+            print(string.format("Player Coords: x=%.2f, y=%.2f z=%.2f", PlayerCoords.x, PlayerCoords.y, PlayerCoords.z))
+            print(string.format("Waypoint Coords: x=%.2f, y=%.2f", WaypointSaveCoord.x, WaypointSaveCoord.y))
+            print(string.format("Distance: %.2f", distance))
 	    end
+
+            if distance < Config.WaypointAutoClearDistance then
+                SetWaypointOff()
+                ClearGpsMultiRoute()
+                SetGpsMultiRouteRender(false)
+				marker = false
+
+                if blip then
+                    RemoveBlip(blip)
+                    blip = nil
+                end
+
+		if Config.Debug then
+                print("Waypoint has been reached and removed.")
+		end
+            end
+        else
+	    if Config.Debug then
+            print("There are no waypoints saved yet.")
+	    end
+        end
+		
+        if Config.AddMarkerWaypointEndPoint and waypoints and #waypoints > 0 and marker then
+	   AddMarker(SaveWaypoint)
 	end
+		
+        if IsUiappActiveByHash(`MAP`) ~= 0 then
+            local mapFocus = DatabindingAddUiItemListFromPath("", "MapFocus")
+            local hoveredName = DatabindingAddDataString(mapFocus, "HoveredName", "")
+            DatabindingWriteDataString(hoveredName, "")
+            DatabindingAddDataBool(mapFocus, "ItemHovered", false)
+        end
+
+        if Config.WaypointClearWithKey then
+            if IsControlJustPressed(0, Config.WaypointClearKey) and IsControlJustPressed(0, Config.WaypointClearKey2) then
+                SetWaypointOff()
+                ClearGpsMultiRoute()
+                SetGpsMultiRouteRender(false)
+				marker = false
+
+                if blip then
+                    RemoveBlip(blip)
+                    blip = nil
+                end
+
+                if Config.Notification.Enable then
+                    TriggerEvent("chat:addMessage", {color = Config.Notification.TitleColor, multiline = true, args = {Config.Notification.Title, Config.Notification.Text}})
+                end
+            end
+        end
     end
 end)
 
-RegisterCommand(Config.WaypointClearCommand, function() -- Clear/Remove Route
+RegisterCommand(Config.WaypointClearCommand, function()
 	SetWaypointOff()
 	ClearGpsMultiRoute()
 	SetGpsMultiRouteRender(false)
+	marker = false
 
 	if blip then
 	RemoveBlip(blip)
@@ -72,7 +141,10 @@ AddEventHandler('onResourceStop', function(resourceName)
 	SetWaypointOff()
 	ClearGpsMultiRoute()
 	SetGpsMultiRouteRender(false)
+	marker = false
 	RemoveBlip(blip)
-		--print("All waypoint/route things cleared")
+	if Config.Debug then
+	print("All waypoint/route things cleared")
+	end
     end
 end)
